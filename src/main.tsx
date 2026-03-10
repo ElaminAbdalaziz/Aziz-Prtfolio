3f34f9901fef6f4fc8d49b696eca3eb5e2cddf45
Abdalaziz Elamin <131213410+ElaminAbdalaziz@users.noreply.github.com>
2026-03-10 03:59:25 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>
+</React.StrictMode>
+);
